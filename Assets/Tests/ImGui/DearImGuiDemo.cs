4bdea0f2e9185aafbf3c008cbc4a2b9595d60fc9
Caleb Gray <hey@calebgray.com>
2020-03-16 14:37:16 -0700

    Added Dear ImGui debug windows for testing.

--- Assets/Tests/ImGui/DearImGuiDemo.cs
+++ Assets/Tests/ImGui/DearImGuiDemo.cs
@@ -11,13 +11,17 @@ public class DearImGuiDemo : MonoBehaviour {
     }
 
     private float _float = 1f;
-    private bool _showDemo;
+    private bool[] _windows = new bool[3];
     private void OnLayout() {
         ImGui.Text($"Hello, world {123}");
         var buf = new byte[1024];
         ImGui.SliderFloat("float", ref _float, 0.0f, 1.0f);
         ImGui.InputText($"Slider: {_float}", buf, (uint) buf.Length);
-        if (ImGui.Button("Demo Window")) _showDemo = !_showDemo;
-        if (_showDemo) ImGui.ShowDemoWindow();
+        if (ImGui.Button("Demo")) _windows[0] = !_windows[0];
+        if (_windows[0]) ImGui.ShowDemoWindow();
+        if (ImGui.Button("About")) _windows[1] = !_windows[1];
+        if (_windows[1]) ImGui.ShowAboutWindow();
+        if (ImGui.Button("Metrics")) _windows[2] = !_windows[2];
+        if (_windows[2]) ImGui.ShowMetricsWindow();
     }
 }
