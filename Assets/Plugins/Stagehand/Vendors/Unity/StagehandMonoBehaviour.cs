4e410e960846aa86cdfea1a38b3583c7ce548b2d
Caleb Gray <hey@calebgray.com>
2020-03-20 19:46:55 -0700

    Continuing to prototype the Auto-Layout feature.

---
+++ Assets/Plugins/Stagehand/Vendors/Unity/StagehandMonoBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using Plugins.Stagehand;
+using UnityEngine;
+
+namespace Plugins.Backstage.Vendors.Unity {
+	public class StagehandMonoBehaviour : MonoBehaviour {
+		// Living and Breathing Code
+		//
+		// If you build it with no uncovered edge cases,
+		// it will do exactly what you programmed it to do...
+		// every time.
+		//
+		// Therefore, we always execute the code as its written
+		// in order to immediately expose edge cases, as they
+		// come up, and they cannot be ignored.
+#if UNITY_EDITOR
+		private void OnValidate() {
+			Stage<MonoBehaviour>.Hand();
+		}
+#else
+		private void OnEnable() {
+			Stage<MonoBehaviour>.Hand();
+		}
+#endif
+	}
+}
